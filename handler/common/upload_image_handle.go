adce835cb6d84146778d94dab0aa357ffb5d74fb
Master Joy <386442876@qq.com>
2025-12-11 14:39:02 +0800

    优化：文件上传接口

--- handler/common/upload_image_handle.go
+++ handler/common/upload_image_handle.go
@@ -4,12 +4,18 @@ import (
 	"go-admin-template/internal/response"
 	"go-admin-template/logic/common"
 	"go-admin-template/svc"
+	"go-admin-template/types"
 
 	"github.com/gin-gonic/gin"
 )
 
 // UploadImageHandle 上传图片
 func UploadImageHandle(c *gin.Context) {
-	resp, err := common.UploadImage(svc.NewServiceContext(c))
+	var req types.UploadImageRequest
+	if err := c.ShouldBind(&req); err != nil {
+		response.HandleResponse(c, nil, err)
+		return
+	}
+	resp, err := common.UploadImage(svc.NewServiceContext(c), &req)
 	response.HandleResponse(c, resp, err)
 }
--- logic/common/upload_image_logic.go
+++ logic/common/upload_image_logic.go
@@ -4,26 +4,21 @@ import (
 	"path/filepath"
 
 	"go-admin-template/svc"
+	"go-admin-template/types"
 
 	"github.com/pkg/errors"
 	"github.com/samber/lo"
 )
 
 // UploadImage 上传图片
-func UploadImage(ctx *svc.ServiceContext) (resp string, err error) {
-	file, err := ctx.GinContext.FormFile("file")
-	if err != nil {
-		ctx.Log.Errorf("%+v", errors.WithStack(err))
-		return
-	}
-
-	ext := filepath.Ext(file.Filename)
+func UploadImage(ctx *svc.ServiceContext, req *types.UploadImageRequest) (resp string, err error) {
+	ext := filepath.Ext(req.File.Filename)
 	if !lo.Contains([]string{".png", ".jpg", ".jpeg"}, ext) {
 		err = errors.New("不支持的文件格式")
 		return
 	}
 
-	open, err := file.Open()
+	open, err := req.File.Open()
 	if err != nil {
 		ctx.Log.Errorf("%+v", errors.WithStack(err))
 		err = errors.New("系统错误")
@@ -32,8 +27,7 @@ func UploadImage(ctx *svc.ServiceContext) (resp string, err error) {
 	defer open.Close()
 
 	// TODO 上传图片至 OSS
-
-	//resp = fmt.Sprintf("https://foo.bar.com/%s", filePath)
+	// resp = fmt.Sprintf("https://foo.bar.com/%s", filePath)
 	resp = "http://tc.masterjoy.top/typory/image-20240828121602348.png"
 	return
 }
